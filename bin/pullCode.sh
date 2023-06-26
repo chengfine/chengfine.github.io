f7ff9b85ba288713fc90a66ea69518857ebbc2ac
李程 <17923477@qq.com>
2023-06-27 00:13:42 +0800

    commit

---
+++ bin/pullCode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 进入项目根目录
+cd /Users/cheng/project/blog
+
+git add .
+
+git commit -m "commit"
+
+git push -u origin main
+
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
-			{ text: "Home", link: "/" },
-			{ text: "Examples", link: "/markdown-examples" },
+			{ text: "首页", link: "/" },
+			{ text: "列表", link: "/markdown-examples" },
 		],
 
 		sidebar: [
