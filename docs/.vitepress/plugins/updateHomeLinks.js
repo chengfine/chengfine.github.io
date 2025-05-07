531d579e9fc7fb0e787cb883e31b2c700c2621bc
cheng <17923477@qq.com>
2025-05-08 00:09:54 +0800

    docs: update content [2025-05-08 00:09:54]

--- docs/.vitepress/plugins/updateHomeLinks.js
+++ docs/.vitepress/plugins/updateHomeLinks.js
@@ -1,22 +1,22 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { getLatestPosts } from '../../src/utils/getLatestPosts.js';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { getLatestPosts } from "../../src/utils/getLatestPosts.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default () => {
   return {
-    name: 'update-home-links',
-    async buildStart() {
-      const basePath = path.join(__dirname, '../../src');
+    name: "update-home-links",
+    buildStart() {
+      const basePath = path.join(__dirname, "../../src");
       const latestPosts = getLatestPosts(basePath);
-      
+
       // 读取首页内容
-      const indexPath = path.join(basePath, 'index.md');
-      let content = fs.readFileSync(indexPath, 'utf-8');
-      
+      const indexPath = path.join(basePath, "index.md");
+      let content = fs.readFileSync(indexPath, "utf-8");
+
       // 更新链接
       content = content.replace(
         /link: \/blog\/.*?\n/g,
@@ -26,10 +26,10 @@ export default () => {
         /link: \/record\/.*?\n/g,
         `link: ${latestPosts.record}\n`
       );
-      console.log('update-home-links 30',content);
-      
+      console.log("update-home-links 30", content);
+
       // 写回文件
       fs.writeFileSync(indexPath, content);
-    }
+    },
   };
-}; 
+};
