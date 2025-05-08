eeecf9a2b35a631133a4d14b5387decb83e2f17f
cheng <17923477@qq.com>
2025-05-08 14:49:12 +0800

    docs: update content [2025-05-08 14:49:12]

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,3 +1,4 @@
+import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vitepress";
 import { nav } from "./nav";
 import { sidebar } from "./sidebar";
@@ -6,9 +7,7 @@ import updateHomeLinks from "./plugins/updateHomeLinks";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   //fav图标
-  head: [
-    ['link',{ rel: 'icon', href: '/images/image.png'}],
-  ],
+  head: [["link", { rel: "icon", href: "/images/image.png" }]],
   base: "/",
   title: "CHENG的博客",
   description: "cheng-github-blog",
@@ -31,15 +30,15 @@ export default defineConfig({
     darkModeSwitchTitle: "切换到深色模式",
     // siteTitle: 'Hello World',
     // siteTitle: false, //标题隐藏
-    logo: '/images/logo-light.svg',
+    logo: "/images/logo-light.svg",
     //开启本地搜索
     search: {
-      provider: "local"
+      provider: "local",
     },
     // 修改文档页脚的文字
     docFooter: {
       prev: "上一页",
-      next: "下一页"
+      next: "下一页",
     },
     footer: {
       message: "Released under the MIT License.",
@@ -48,10 +47,27 @@ export default defineConfig({
   },
 
   vite: {
-    plugins: [updateHomeLinks()],
+    plugins: [
+      AutoImport({
+        imports: [
+          "vue",
+          // {
+          //   "naive-ui": [
+          //     "useDialog",
+          //     "useMessage",
+          //     "useNotification",
+          //     "useLoadingBar",
+          //   ],
+          // },
+        ],
+        dts: true, // 生成类型声明文件
+        include: [/\.md$/, /\.vue$/],
+      }),
+      updateHomeLinks(),
+    ],
     server: {
       port: 3000, // 设置端口为3000
-      open: true  // 自动打开浏览器
-    }
-  }
+      open: false, // 自动打开浏览器
+    },
+  },
 });
